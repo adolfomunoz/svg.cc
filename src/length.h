aa23966d95f177e545e01f68d1ce7b09c7fb62f9
adolfomunoz <adolfo@unizar.es>
2022-02-21 15:18:56 +0100

    Modified length definition so we can make calculations at least with absolute measurements.

--- src/length.h
+++ src/length.h
@@ -1,40 +1,52 @@
 #pragma once 
 #include <iostream>
+#include <patterns/patterns.h>
 
 namespace svg {
 
-enum class LengthUnit {
-    none, em, ex, px, in, cm, mm, pt, pc, percentage
+class LengthUnitBase : public pattern::SelfRegisteringReflectableBase {
+public:
+    virtual float ratio() const = 0;
+    virtual const char* name() const = 0;
+    bool is_absolute() const { return ratio()>0.0f; }
 };
 
-std::ostream& operator<<(std::ostream& os, LengthUnit unit) {
-    switch (unit) {
-        case LengthUnit::none : os<<"";   break;
-        case LengthUnit::em   : os<<"em"; break;
-        case LengthUnit::ex   : os<<"ex"; break;
-        case LengthUnit::px   : os<<"px"; break;
-        case LengthUnit::in   : os<<"in"; break;
-        case LengthUnit::cm   : os<<"cm"; break;
-        case LengthUnit::mm   : os<<"mm"; break;
-        case LengthUnit::pt   : os<<"pt"; break;
-        case LengthUnit::pc   : os<<"pc"; break;
-        case LengthUnit::percentage   : os<<"%"; break;
-    }
-    return os;
+class LengthUnit : public pattern::Pimpl<LengthUnitBase> {
+public:
+    using pattern::Pimpl<LengthUnitBase>::Pimpl;
+    using pattern::Pimpl<LengthUnitBase>::operator=;
+    float ratio() const override { return this->impl()->ratio(); }
+    const char* name() const override { return this->impl()->name(); }
+};
+
+#define LENGTH_UNIT(CLASS,NAME,RATIO) \
+    class LengthUnit##CLASS : public pattern::SelfRegisteringReflectable<LengthUnit##CLASS, LengthUnitBase> {\
+        public: \
+            static const char* type_name() { return NAME; } \
+            const char* name() const override { return NAME; } \
+            float ratio() const override { return RATIO; } \
+    };
+
+LENGTH_UNIT(None,"",1)
+LENGTH_UNIT(Px,"px",1)
+LENGTH_UNIT(Em,"em",-1)
+LENGTH_UNIT(Ex,"ex",-1)
+LENGTH_UNIT(In,"in",96)
+LENGTH_UNIT(Cm,"cm",37.795)
+LENGTH_UNIT(Mm,"mm",377.95)
+LENGTH_UNIT(Pt,"pt",1.33333333)
+LENGTH_UNIT(Pc,"pc",16)
+LENGTH_UNIT(Percentage,"%",-1)
+
+
+
+std::ostream& operator<<(std::ostream& os, const LengthUnit& unit) {
+    os<<unit.name(); return os;
 };
 
 std::istream& operator>>(std::istream& is, LengthUnit& unit) {
     std::string token; is>>token;
-    if (token == "") unit = LengthUnit::none;
-    else if (token == "em") unit = LengthUnit::em;
-    else if (token == "ex") unit = LengthUnit::ex;
-    else if (token == "px") unit = LengthUnit::px;
-    else if (token == "in") unit = LengthUnit::in;
-    else if (token == "cm") unit = LengthUnit::cm;
-    else if (token == "mm") unit = LengthUnit::mm;
-    else if (token == "pt") unit = LengthUnit::pt;
-    else if (token == "pc") unit = LengthUnit::pc;
-    else if (token == "%") unit = LengthUnit::percentage;
+    unit.set_type(token);
 
     return is;
 };
@@ -43,10 +55,32 @@ class Length {
 	float q;
 	LengthUnit u;
 public:
-	Length(float quantity = 1, const LengthUnit& unit = LengthUnit::none) : q(quantity), u(unit) { }
-	float quantity() const { return q; }
-    LengthUnit unit() const { return u; }
+    Length(float quantity = 1) : q(quantity), u(LengthUnitNone()) {}
+	Length(float quantity, const LengthUnit& unit) : q(quantity), u(unit) { }
+	float quantity() const noexcept { return q; }
+    const LengthUnit& unit() const noexcept { return u; }
+
+    Length operator+(const Length& that) const noexcept {
+        return Length(quantity()+that.quantity()*that.unit().ratio()/unit().ratio(),unit());
+    }
+
+    Length operator-(const Length& that) const noexcept {
+        return Length(quantity()-that.quantity()*that.unit().ratio()/unit().ratio(),unit());
+    }
+
+    Length operator*(float that) const noexcept {
+        return Length(quantity()*that,unit());
+    }
     
+    Length operator/(float that) const noexcept {
+        return Length(quantity()/that,unit());
+    }
+
+    float operator/(const Length& length) const noexcept {
+        return (quantity()*unit().ratio())/(length.quantity()*length.unit().ratio());
+    }
+ 
+ 
     friend std::istream& operator>>(std::istream& is, Length& length);
 };
 
