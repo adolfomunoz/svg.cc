1dfe7025f7c5549b2295193114866d049f2dd5a5
Adolfo <adolfo@unizar.es>
2023-04-12 16:22:51 +0200

    Tweaked plots so that they calculate the axis account for markersize if it is relevant (basically, they use the scatter plot axis if markers are visible).

--- plot/src/plot.cc
+++ plot/src/plot.cc
@@ -39,16 +39,19 @@ svg::Element Plot::plot(const Transform& xscale, const Transform& yscale) const 
     }
 }
 std::array<float,4> Plot::axis() const noexcept {
-    if (x.empty() || y.empty()) return std::array<float,4>{0.0f,0.0f,0.0f,0.0f};
-    std::array<float,4> a{x.front(),x.front(),y.front(),y.front()};
-    std::list<float>::const_iterator ix, iy;
-    for (ix = x.begin(), iy = y.begin(); (ix != x.end()) && (iy != y.end()); ++ix, ++iy) {
-        if (*ix < a[0]) a[0] = *ix;
-        if (*ix > a[1]) a[1] = *ix;
-        if (*iy < a[2]) a[2] = *iy;
-        if (*iy > a[3]) a[3] = *iy;
+    if (markers_set) return markers.axis();
+    else {
+        if (x.empty() || y.empty()) return std::array<float,4>{0.0f,0.0f,0.0f,0.0f};
+        std::array<float,4> a{x.front(),x.front(),y.front(),y.front()};
+        std::list<float>::const_iterator ix, iy;
+        for (ix = x.begin(), iy = y.begin(); (ix != x.end()) && (iy != y.end()); ++ix, ++iy) {
+            if (*ix < a[0]) a[0] = *ix;
+            if (*ix > a[1]) a[1] = *ix;
+            if (*iy < a[2]) a[2] = *iy;
+            if (*iy > a[3]) a[3] = *iy;
+        }
+        return a;
     }
-    return a;
 }
 
 Plot& Plot::linewidth(float f) noexcept { linewidth_=f; return *this; }
